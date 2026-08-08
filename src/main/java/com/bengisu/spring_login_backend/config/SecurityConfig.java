380c1efb61314fc81bb0cd73e56d55da5c56a5d7
bengisu777 <bengisuinfo@gmail.com>
2026-08-08 23:05:22 +0300

    JwtAuthFilter eklentisi yapıldı

--- src/main/java/com/bengisu/spring_login_backend/config/SecurityConfig.java
+++ src/main/java/com/bengisu/spring_login_backend/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.bengisu.spring_login_backend.config;
 
+import com.bengisu.spring_login_backend.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -15,7 +23,8 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/health", "/api/register", "/api/login")
-                        .permitAll().anyRequest().authenticated());
+                        .permitAll().anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
